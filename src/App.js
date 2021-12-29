afb8d93bbb7258d43eefc07d966c939718c882df
xedice-mehdiyeva <xedice.mehdiyeva00@gmail.com>
2021-12-29 12:23:31 -0800

    Navbar yarimciq versiya

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 
-import './App.css';
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from './components/Navbar'
+
 
 function App() {
   return (
-    <h1>hello</h1>
+    <>
+    <Navbar/>
+    </>
   );
 }
 
