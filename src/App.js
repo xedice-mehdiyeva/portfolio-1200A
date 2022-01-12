ddb1c7c4412fbeb3528d1bb72700a4b7f249b033
xedice-mehdiyeva <xedice.mehdiyeva00@gmail.com>
2022-01-12 13:35:31 -0800

    Contact us yaradildi

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Navbar from './components/Navbar'
-import Header from './components/Header';
+import Header from './components/Header'
+import Contactus from './components/Contactus'
 
 function App() {
   return (
     <>  
    <Navbar />
    <Header />
+   <Contactus />
    </>
   );
 }
